3577d911e6a5eb6a8c152e7db151425c79c626f8
Charlie-UWG <charlie.shimonishi.uwg@gmail.com>
2021-03-13 08:35:16 +0900

    _App.tsxをTypescriptで書き換え

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Practice1 from '../practicies/Practice1'
 import Practice2 from '../practicies/Practice2'
 import Practice3 from '../practicies/Practice3'
 import Practice4 from '../practicies/Practice4'
+import HomePage from '../testPages/HomePage'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Practice2 />
       <Practice3 />
       <Practice4 />
+      <HomePage />
     </div>
   )
 }
